800d9afd36fcfcd57d8d458677dddd6e3071619d
sr-0521 <159591069+sr-0521@users.noreply.github.com>
2025-11-06 17:50:21 -0500

    Added initial template

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
 function App() {
-  
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>Hi, I'm Saad Rizvi</h1>
+      <h2>Welcome to my portfolio</h2>
+      <p>I'm a computer science studnet passionate about building interactive web applications</p>
+    </div>
+  );
 }
+
+export default App;
